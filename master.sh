83a6c22eb9a57921190edf1996124faab6269380
akif batur <akifbatur@protonmail.com>
2025-01-17 09:07:57 +0300

    monitoring added

--- master.sh
+++ master.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#multipass delete master && multipass purge && multipass launch 24.04 --name master -c 4 -m 12G -d 15G && multipass shell master
+#multipass delete master && multipass purge && multipass launch 24.04 --name master -c 5 -m 14G -d 30G && multipass shell master
 
 # Function to print colored messages
 print_message() {
@@ -122,7 +122,6 @@ initialize_master() {
     mkdir -p $HOME/.kube
     sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
     sudo chown $(id -u):$(id -g) $HOME/.kube/config
-    print_message "Kubernetes master initialized."
     measure_time $start_time
 }
 
@@ -135,10 +134,10 @@ install_calico() {
     kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.25.1/manifests/tigera-operator.yaml
     kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.25.1/manifests/custom-resources.yaml
     kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
-    print_message "Calico network plugin installed."
-    measure_time $start_time
     print_message "Waiting for Calico pods to be up..."
     wait_for_pods
+    print_message "Calico network plugin installed."
+    measure_time $start_time
 }
 
 # Wait for all pods in the given namespace to be running
@@ -147,7 +146,6 @@ wait_for_pods() {
     while true; do
         # Pods that are not in Running, Succeeded, or Completed status in any namespace
         PODS=$(kubectl get pods --all-namespaces --field-selector=status.phase!=Running,status.phase!=Succeeded,status.phase!=Completed --no-headers)
-        
         if [ -z "$PODS" ]; then
             print_message "All pods in all namespaces are in a healthy state."
             break
@@ -163,7 +161,6 @@ configure_metallb() {
     kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.14.9/config/manifests/metallb-native.yaml
     print_message "Waiting for MetalLB pods to be up..."
     wait_for_pods
-
     cat <<EOF | kubectl apply -f -
 apiVersion: metallb.io/v1beta1
 kind: IPAddressPool
@@ -192,72 +189,72 @@ install_ingress() {
     local start_time=$(date +%s)
     print_message "Installing NGINX Ingress..."
     kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/cloud/deploy.yaml
+    wait_for_pods
     print_message "Waiting for NGINX Ingress pods to be up..."
     kubectl patch svc ingress-nginx-controller -n ingress-nginx -p '{"spec": {"type": "LoadBalancer"}}'
     print_message "NGINX Ingress Controller installed."
-    wait_for_pods
-#    echo "waiting for additional 1 minute"
-#    sleep 1m
-    # Apply the example Ingress configuration
-    #cat <<EOF | kubectl apply -f -
-    #apiVersion: networking.k8s.io/v1
-    #kind: Ingress
-    #metadata:
-    #  name: example-ingress
-    #  namespace: default
-    #  annotations:
-    #    nginx.ingress.kubernetes.io/rewrite-target: /
-    #spec:
-    #  rules:
-    #  - host: example.local
-    #    http:
-    #      paths:
-    #      - path: /
-    #        pathType: Prefix
-    #        backend:
-    #          service:
-    #            name: example-service
-    #            port:
-    #              number: 80
-    #EOF
-    print_message "Example Ingress applied."
     measure_time $start_time
 }
 
-# Main function to execute the tasks
-main() {
+install_monitoring() {
     local start_time=$(date +%s)
-    disable_firewall
-    install_dependencies
-    install_helm
-    configure_kernel
-    disable_swap
-    install_containerd
-    install_kubernetes
-    initialize_master
-    install_calico
-    MASTER_IP=$(hostname -I | awk '{print $1}')
-    configure_metallb "${MASTER_IP}-${MASTER_IP}"  # Example IP range
-    install_ingress
-    print_message "To join worker nodes, run the following command on each worker node:"
-    kubeadm token create --print-join-command
-    echo "https://$MASTER_IP"
-    #Completed görünen pod'ları sil'
-    kubectl delete pod --field-selector=status.phase=Completed -A
-    #Dashboard
-    # Add kubernetes-dashboard repository
-    helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
-    # Deploy a Helm Release named "kubernetes-dashboard" using the kubernetes-dashboard chart
-    helm upgrade --install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard --create-namespace --namespace kubernetes-dashboard
-cat <<EOF > dashboard-kong.yaml
-kong:
-  proxy:
-    type: NodePort
-  http:
-    enabled: true
+    kubectl create namespace monitoring
+    helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+    helm repo update
+    helm install kubeprostack --namespace monitoring prometheus-community/kube-prometheus-stack
+    print_message "Waiting for monitoring pods to be up..."
+    wait_for_pods
+    #kubectl --namespace monitoring port-forward svc/kubeprostack-kube-promethe-prometheus 9090
+    #kubectl --namespace monitoring port-forward svc/kubeprostack-kube-promethe-alertmanager 9093
+    #kubectl --namespace monitoring port-forward svc/kubeprostack-grafana 8080:80
+    kubectl patch svc kubeprostack-kube-promethe-prometheus -n monitoring -p '{"spec": {"type": "NodePort", "ports": [{"port": 9090, "nodePort": 30090}]}}'
+    kubectl patch svc kubeprostack-kube-promethe-alertmanager -n monitoring -p '{"spec": {"type": "NodePort", "ports": [{"port": 9093, "nodePort": 30093}]}}'
+    kubectl patch svc kubeprostack-grafana -n monitoring -p '{"spec": {"type": "NodePort", "ports": [{"port": 80, "nodePort": 30080}]}}'
+cat <<EOF | kubectl apply -f -
+apiVersion: rbac.authorization.k8s.io/v1
+kind: Role
+metadata:
+  namespace: monitoring
+  name: prometheus-role
+rules:
+  - apiGroups: [""]
+    resources:
+      - pods
+      - services
+      - endpoints
+      - nodes
+      - nodes/proxy
+      - configmaps
+    verbs: ["get", "list", "watch"]
 EOF
-    helm upgrade kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard -f dashboard-kong.yaml -n kubernetes-dashboard
-cat <<EOF > dashboard-user.yaml
+cat <<EOF | kubectl apply -f -
+apiVersion: rbac.authorization.k8s.io/v1
+kind: RoleBinding
+metadata:
+  name: prometheus-rolebinding
+  namespace: monitoring
+subjects:
+  - kind: ServiceAccount
+    name: default
+    namespace: monitoring
+roleRef:
+  kind: Role
+  name: prometheus-role
+  apiGroup: rbac.authorization.k8s.io
+EOF
+    #kubectl get secret kubeprostack-grafana -n monitoring -o jsonpath="{.data.admin-password}" | base64 --decode ; echo
+    measure_time $start_time
+}
+
+install_dashboard(){
+    local start_time=$(date +%s)
+    print_message "Installing Kubernetes Dashboard..."
+    helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
+    helm upgrade --install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard --create-namespace --namespace kubernetes-dashboard
+    wait_for_pods
+    #kubectl patch svc kubernetes-dashboard-web -n kubernetes-dashboard -p '{"spec": {"type": "NodePort"}}'
+    kubectl patch svc kubernetes-dashboard-kong-proxy -n kubernetes-dashboard -p '{"spec": {"type": "NodePort"}}'
+cat <<EOF | kubectl apply -f -
 apiVersion: v1
 kind: ServiceAccount
 metadata:
@@ -276,31 +273,58 @@ subjects:
 - kind: ServiceAccount
   name: admin-user
   namespace: kubernetes-dashboard
----
-apiVersion: v1
-kind: Secret
-metadata:
-  name: admin-user
-  namespace: kubernetes-dashboard
-  annotations:
-    kubernetes.io/service-account.name: "admin-user"
-type: kubernetes.io/service-account-token
 EOF
-    kubectl apply -f dashboard-user.yaml
-    kubectl get secret admin-user -n kubernetes-dashboard -o jsonpath={".data.token"} | base64 -d
-    kubectl create namespace monitoring
-    helm install my-grafana grafana/grafana --namespace monitoring
-    
-    echo "Graphana username: admin"
-    echo "Graphana admin password:"
-    kubectl get secret --namespace monitoring my-grafana -o jsonpath="{.data.admin-password}" | base64 --decode ; echo
-   
-    echo ""
+    kubectl -n kubernetes-dashboard create token admin-user
+    print_message "Kubernetes Dashboard installed."
+    measure_time $start_time
+}
+
+# Main function to execute the tasks
+main() {
+    local start_time=$(date +%s)
+    MASTER_IP=$(hostname -I | awk '{print $1}')
+    disable_firewall
+    install_dependencies
+    install_helm
+    configure_kernel
+    disable_swap
+    install_containerd
+    install_kubernetes
+    initialize_master
+    install_calico
+    configure_metallb "${MASTER_IP}-${MASTER_IP}"
+    install_ingress
+    install_monitoring
+    install_dashboard 
+    kubectl get pods -A --field-selector=status.phase=Succeeded -o custom-columns="NAMESPACE:.metadata.namespace,NAME:.metadata.name" --no-headers | awk '{print "kubectl delete pod -n "$1" "$2}' | sh
     local end_time=$(date +%s)
     local elapsed=$((end_time - start_time))
     local minutes=$((elapsed / 60))
     local seconds=$((elapsed % 60))
-    print_message "Total script execution time: $minutes minutes and $seconds seconds"
+    GRAFANA_PORT=$(kubectl get svc -n monitoring kubeprostack-grafana -o jsonpath='{.spec.ports[0].nodePort}')
+    PROMETHEUS_PORT=$(kubectl get svc -n monitoring kubeprostack-kube-promethe-prometheus -o jsonpath='{.spec.ports[0].nodePort}')
+    ALERTMANAGER_PORT=$(kubectl get svc -n monitoring kubeprostack-kube-promethe-alertmanager -o jsonpath='{.spec.ports[0].nodePort}')
+    DASHBOARD_PORT=$(kubectl get svc -n kubernetes-dashboard kubernetes-dashboard-kong-proxy -o jsonpath='{.spec.ports[0].nodePort}')
+    WORKER_JOIN=$(kubeadm token create --print-join-command)
+    DASHBOARD_TOKEN=$(kubectl -n kubernetes-dashboard create token admin-user)
+    print_message "Shogun says:"
+    touch app_ip_addr
+    echo "Grafana: http://$MASTER_IP:$GRAFANA_PORT" > app_ip_addr
+    echo "Grafana username: admin" >> app_ip_addr
+    echo "Grafana password: prom-operator" >> app_ip_addr
+    echo "Prometheus: http://$MASTER_IP:$PROMETHEUS_PORT" >> app_ip_addr
+    echo "Alert Manager: http://$MASTER_IP:$ALERTMANAGER_PORT" >> app_ip_addr
+    echo "Dashboard: https://$MASTER_IP:$DASHBOARD_PORT" >> app_ip_addr
+    echo "" >> app_ip_addr
+    echo "Dashboard Bearer Token: $DASHBOARD_TOKEN" >> app_ip_addr
+    echo "" >> app_ip_addr
+    echo "Worker join command:" $WORKER_JOIN >> app_ip_addr
+    local end_time=$(date +%s)
+    local elapsed=$((end_time - start_time))
+    local minutes=$((elapsed / 60))
+    local seconds=$((elapsed % 60))
+    echo "Total script execution time: $minutes minutes and $seconds seconds"
+    print_message "All set"
 }
 
 # Run the main function
